97704002c06599f19b31b31fe50f8fe3f13a4394
Matthew J Palathingal <mattjose@umich.edu>
2020-08-10 12:51:28 -0400

    added bot moves

--- Hand_and_Brain.py
+++ Hand_and_Brain.py
@@ -1,14 +1,21 @@
 from stockfish import Stockfish
+stockfish_path = "C:/Users/mpala/Downloads/stockfish-11-win/stockfish-11-win/Windows/stockfish_20011801_x64"
+
 
 def getMove():
     move = input("Enter next move: ")
     return move
 
+
 def main():
-    stockfish = Stockfish("C:/Users/mpala/Downloads/stockfish-11-win/stockfish-11-win/Windows/stockfish_20011801_x64")
+    stockfish = Stockfish(stockfish_path)
     moves = []
     while(stockfish.get_evaluation() != {"type":"mate", "value":0}):
+        
+        # display updated board
         print(stockfish.get_board_visual())
+        
+        # make moves as white
         next_move = getMove()
         if stockfish.is_move_correct(next_move):
             moves.append(next_move)
@@ -16,5 +23,14 @@ def main():
             print("not a valid move")
             getMove()
         stockfish.set_position(moves)
+        print(stockfish.get_board_visual())
+
+        # get bot's move
+        bot_move = stockfish.get_best_move()
+        print("Bot moves: " + bot_move)
+        moves.append(bot_move)
+        stockfish.set_position(moves)
+
+
 if __name__ == "__main__":
     main()
